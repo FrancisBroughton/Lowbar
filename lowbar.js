b2ffc9a85657c0c11ad54eea84734efbe3e96cb2
FrancisBroughton <iamfrancisbroughton@gmail.com>
2017-11-10 16:23:11 +0000

    it returns the first element in an array

--- lowbar.js
+++ lowbar.js
@@ -4,4 +4,9 @@ _.identity = function(x) {
   return x;
 };
 
+_.first = function(arr, n = 1) {
+  if (n === 1) return arr[0];
+  return arr.slice(0, n);
+};
+
 module.exports = _;
--- spec/lowbar.js
+++ spec/lowbar.js
@@ -25,5 +25,11 @@ describe('#identity', () => {
       var input1 = {name: 'Francis'};
       expect(_.identity(input1)).to.equal(input1);
     });
+describe('#first', () => {
+  it('returns the first element in an array', () => {
+    var input = [3,7,2];
+    expect(_.first(input)).to.equal(3)
+  })
+})
   });
 });
