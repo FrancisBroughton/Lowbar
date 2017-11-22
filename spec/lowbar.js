b8a9765d509bbde8f86046790b7f7181319feac0
FrancisBroughton <iamfrancisbroughton@gmail.com>
2017-11-22 09:45:23 +0000

    sinon has been installed

--- spec/lowbar.js
+++ spec/lowbar.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var expect = require('chai').expect;
+var sinon = require('sinon');
 var _ = require(path.join(__dirname, '..', './lowbar.js'));
 
 describe('_', function () {
